662ecf4c8064a3770d86f0092350406d67758a1a
vichetchenda <vichetchenda.sakhan@wingmoney.com>
2024-09-27 14:31:43 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 import { PopupWidget }  from "@/components/PopupWidget";
-
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +29,7 @@ export default function RootLayout({
           <Footer />
           <PopupWidget />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
